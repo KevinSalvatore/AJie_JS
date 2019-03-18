6a5937422d40b0bdf9975d51b71f7e7492715b20
AJie <kevinsalvatore@qq.com>
2019-03-18 13:09:48 +0800

    Something new about SVG!

--- Day_19/js/index.js
+++ Day_19/js/index.js
@@ -10,7 +10,16 @@ musicBtn.addEventListener("click", () => {
   }
   musicBtn.classList.toggle("off");
 });
-
+const paras = document.querySelector(".paras");
 var swiper = new Swiper(".swiper-container", {
-  direction: "vertical"
+  direction: "vertical",
+  on: {
+    slideChangeTransitionEnd: function() {
+      if (this.activeIndex === 1) {
+        paras.classList.add("z-enter");
+      } else {
+        paras.classList.remove("z-enter");
+      }
+    }
+  }
 });
---
+++ Day_20/js/index.js
@@ -0,0 +1,9 @@
+// let pathLength = document.querySelectorAll('path');
+// let ulEl = document.createElement('ul');
+// pathLength.forEach((item, index) => {
+//   let itemLength = Math.ceil(item.getTotalLength());
+//   let liEl = document.createElement('li');
+//   liEl.textContent = `path-${index + 1}: ${itemLength}`;
+//   ulEl.appendChild(liEl);
+// });
+// document.body.appendChild(ulEl);
