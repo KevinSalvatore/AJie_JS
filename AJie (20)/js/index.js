2a781a9b00bf85af778f9d7a4888215c73260669
AJie <kevinsalvatore@qq.com>
2019-04-03 14:08:48 +0800

    Something new!

--- AJie (20)/js/index.js
+++ AJie (20)/js/index.js
@@ -1,9 +1,5 @@
-// let pathLength = document.querySelectorAll('path');
-// let ulEl = document.createElement('ul');
-// pathLength.forEach((item, index) => {
-//   let itemLength = Math.ceil(item.getTotalLength());
-//   let liEl = document.createElement('li');
-//   liEl.textContent = `path-${index + 1}: ${itemLength}`;
-//   ulEl.appendChild(liEl);
-// });
-// document.body.appendChild(ulEl);
+let pathLength = document.querySelectorAll('path');
+pathLength.forEach((item, index) => {
+  let itemLength = Math.ceil(item.getTotalLength());
+  console.log(itemLength);
+});
---
+++ AJie (46)/README.md
@@ -0,0 +1,12 @@
+# Stylus
+
+1. @import
+
+   文件引入，一个文件做一件事，有利于代码的维护和管理，多人协作。
+
+   - /base/reset（基础的 reset）
+     - base是核心
+   - @import让我们可以做文件的分离和管理，多人协作，目录让我们看到思想及野心。
+   - variable 当是一个大项目时，有大量的变量需要维护，variable目录
+   - weui.styl是入口文件，@import负责各个部分
+   - 
