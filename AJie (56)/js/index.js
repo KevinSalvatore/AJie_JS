fbefdf63c0bc0a0f742943aadf09432154ffc747
AJie <kevinsalvatore@qq.com>
2019-04-09 20:42:50 +0800

    Something new!

---
+++ AJie (56)/js/index.js
@@ -0,0 +1,54 @@
+function AJ(arr) {
+  this.nodeList = arr;
+}
+
+AJ.prototype.css = function(styleObj) {
+  this.nodeList.forEach(node => {
+    for (const key in styleObj) {
+      eval("node.style." + key + " = styleObj." + key);
+    }
+  });
+  return this;
+};
+
+AJ.prototype.html = function(html) {
+  this.nodeList.forEach(node => {
+    node.innerHTML = html;
+  });
+  return this;
+};
+
+var $ = function(parameter) {
+  if (typeof parameter === "function") {
+    let callback = parameter;
+    document.addEventListener("DOMContentLoaded", function() {
+      callback();
+    });
+  } else if (typeof parameter === "string") {
+    let selector = parameter;
+    return new AJ(DOMEle(selector));
+  } else {
+    return;
+  }
+};
+
+function DOMEle(string) {
+  let selectors = string.split(" ");
+  let range = [document];
+  selectors.forEach(selector => {
+    let temp = [];
+    range.forEach(ele => {
+      temp = temp.concat(Array.from(ele.querySelectorAll(selector)));
+    });
+    range = temp;
+  });
+  return range;
+}
+
+$(".container .content div")
+  .css({
+    width: "200px",
+    height: "200px",
+    backgroundColor: "red"
+  })
+  .html("我爱你！");
